733d5de4e4056263e493478fce5b4960e2aa2037
Ugo <ugo.aku@sensei.com>
2023-10-24 11:43:22 +0000

    NEW ENTRY

---
+++ greek_gods.py
@@ -0,0 +1,10 @@
+greekgod = str(input("Pray to a small god:"))
+
+if greekgod == "Zeus":
+    print("The Chief of all gods")
+elif greekgod == "Athena":
+  print("The god of love")
+elif greekgod == "Cronus":
+  print ("Titan god of harvest")
+else:
+  print("No classification for the gods")
---
+++ hades.py
@@ -0,0 +1,14 @@
+n = int(input("Enter any number: "))
+if n % 2 == 0:
+    print ("Number is divisible by 2")
+elif n % 3 == 0:
+    print ("Number is divisible by 3")
+else:
+    print("What cannot be done does not exist")
+
+
+n = int(input("Enter any number: "))
+if n % 2 == 0 and n % 3 == 0:
+ print ("Number is divisible by 2 and divisible by 3")
+else:
+ print("What cannot be done does not exist")
